1aa0c4c7900558416fad72515dce02512f507628
Trần Đăng Tài <155811195+TaiTranDang145@users.noreply.github.com>
2026-05-02 17:43:47 +0700

    Update solution for unet-decoder-block

--- unet/unet-decoder-block/unet-decoder-block.py
+++ unet/unet-decoder-block/unet-decoder-block.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+def skip_connection(skip, h, w):
+    B,H,W,C = skip.shape
+    h_start = (H - h)//2
+    w_start = (W - w) //2
+    return skip[:,h_start:h_start+h, w_start:w_start+w,:]
+
+
 def unet_decoder_block(x: np.ndarray, skip: np.ndarray, out_channels: int) -> np.ndarray:
     """
     Returns zero array with correct shape.
@@ -8,11 +15,13 @@ def unet_decoder_block(x: np.ndarray, skip: np.ndarray, out_channels: int) -> np
     B,H,W,C = x.shape
     H1 = H*2
     W1 = W*2
-    C = out_channels
-    H2 = H1 - 2
-    W2 = W1 -2
-    H3 = H2 -2
-    W3 = W2 -2
-    out = np.zeros((B,H3,W3,C))
-    return out
+    C1 = C//2
+    x1 = np.zeros((B,H1,W1,C1))
+    x2 = skip_connection(skip, H1,W1)
+    x3 = np.zeros((B,H1,W1,C1 + x2.shape[-1]))
     
+    H_out = H1 - 4
+    W_out = W1 - 4
+    out = np.zeros((B, H_out, W_out, out_channels))
+
+    return out
